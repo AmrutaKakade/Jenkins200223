9c847a3b355d5dff6bf486566066ef31c54120e3
Amruta <amrukakade26@gmail.com>
2023-02-22 07:36:47 +0530

    addong .class file

---
+++ JobChain_01.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class JobChain_01 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("This is job chain 01");
+}
+}
---
+++ JoobChain_02.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class JoobChain_02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("This is job chain 02");
+}
+}
